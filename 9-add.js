9914c72988af3705cbd581784e96318a853e0ab3
Margaret142 <tabbywange75@gmail.com>
2025-05-27 09:34:27 +0300

    Correct output

--- 9-add.js
+++ 9-add.js
@@ -1,12 +1,8 @@
 
 const add = (a, b) => {
-  const number1 = parseInt(a);
-  const number2 = parseInt(b);
-  if (isNaN(number1) || isNaN(number2)) {
-    console.log("Both inputs must be valid integers");
-  } else {
-    console.log(`The sum of ${number1} and ${number2} is: ${number1 + number2}`);
-  }
+    console.log(`${a + b}`);
 };
 
-add(5, 10);
+add()
+add(1, 7);
+add(13, 89);
